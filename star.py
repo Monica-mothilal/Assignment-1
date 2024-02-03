6cc0fb66d604719af676a932b7492269bee29f93
Monica-mothilal <156389179+Monica-mothilal@users.noreply.github.com>
2024-02-03 15:25:39 +0530

    star.py

--- star.py
+++ star.py
@@ -1,8 +1,8 @@
-def star_pattern2(n):
+def star_pattern(n):
     for i in range(n, 0, -1 ):
         for j in range(1, i + 1):
             print("* ", end="")
         print()
 n=6
-star_pattern2(n)
+star_pattern(n)
 
